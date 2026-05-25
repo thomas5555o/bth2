989f7403ca288ff2db0f983239ba1b86e6c30160
thomas5555o <erfmdl095@gmail.com>
2026-05-25 19:04:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-# =========================
+# ========================= 
 # CONFIG EMAIL 
 # =========================
 
