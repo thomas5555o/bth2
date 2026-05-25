e592ad4d9aa5b74fc328d3cee380bfcb648366fe
thomas5555o <erfmdl095@gmail.com>
2026-05-25 15:19:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -499,8 +499,8 @@ if st.button("🎲 Lancer la roue"):
         )
 
         st.success(
-            f"{selected_color}\\"
-            f"{text_selected_color}"
+            f"{selected_color}\n"
+            {text_selected_color}
         )
 
     except Exception as e:
